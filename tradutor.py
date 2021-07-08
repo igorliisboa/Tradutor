ca5a7f2c49a19bcbe05d3ab06b60dd5a284772de
Igor Lisboa <kazzeditor@gmail.com>
2021-07-08 16:33:16 -0300

    Tradutor

---
+++ tradutor.py
@@ -0,0 +1,9 @@
+#Tradutor de texto em Python
+
+from translate import Translator
+
+s = Translator(from_lang="CHOOSE A LANGUAGE",to_lang="CHOOSE A LANGUAGE")
+
+res = s.translate("PUT THE TEXT HERE")
+
+print(res)
